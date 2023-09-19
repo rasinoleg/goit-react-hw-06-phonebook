cce120071dbbdb4432b80352cee7682421a9ff26
rasinoleg <rasinoleg3@gmail.com>
2023-09-19 23:28:43 +0300

    react 6-phonebook

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,43 +15,16 @@ export const store = configureStore({
   reducer: {
     contacts: persistedReducer, 
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      thunk: true,
-      serializableCheck: false, 
-    }).concat(thunk),
+  middleware: getDefaultMiddleware({
+    thunk: true,
+    serializableCheck: false, 
+  }).concat(thunk),
 });
 
 export const persistor = persistStore(store); 
 
 export default store;
 
-
-
-
-
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import contactReducer from './contactSlice';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage'; 
-
-// const persistConfig = {
-//   key: 'root', 
-//   storage, 
-// };
-
-// const persistedReducer = persistReducer(persistConfig, contactReducer);
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: persistedReducer, 
-//   },
-// });
-
-// export const persistor = persistStore(store); 
-
-// export default store;
 
 
 
